c38d2a0b1bfb225a9501684e97dba0c19a86740a
Liar1124 <89758374+Liar1124@users.noreply.github.com>
2022-01-01 07:57:14 +0800

    Add files via upload

---
+++ 1. 算法部分/第1章 高精度计算/4、阶乘和(sum) .cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+void getni(int *ni, int n)
+{
+	int add = 0;
+	memset(ni, 0, sizeof(ni));
+	ni[0] = 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int bit = 0; bit < n; bit++)
+		{
+			ni[bit] *= i;
+			ni[bit] += add;
+			add = ni[bit] / 10;
+			ni[bit] %= 10;
+		}
+	}
+	return;
+}
+
+void add(int *sum, int *ni)
+{
+	int add = 0;
+	for (int i = 0; i < 49; i++)
+	{
+		sum[i] += ni[i];
+		sum[i + 1] += sum[i] / 10;
+		sum[i] %= 10;
+	}
+}
+
+int main()
+{
+	int sum[50], ni[50];
+	memset(sum, 0, sizeof(sum));
+	memset(ni, 0, sizeof(ni));
+	int input;
+	cin >> input;
+
+	for (int i = 1; i <= input; i++)
+	{
+		getni(ni, i);
+		add(sum, ni);
+	}
+
+	for (int i = 49, zero = true; i >= 0; i--)
+	{
+		if (sum[i] == 0 && zero)
+		{
+			continue;
+		}
+		if (sum[i] != 0)
+			zero = false;
+		cout << sum[i];
+	}
+
+	return 0;
+}
